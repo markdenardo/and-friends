58fbc1a5b07ecb4424f469073979aaed6fe14146
MDN <mark.denardo@gmail.com>
2023-11-29 10:22:06 -0500

    Update wednesday.js

--- wednesday.js
+++ wednesday.js
@@ -2,13 +2,9 @@
 // m8 minimalism
 // by MDN
 // https://instagram/markdenardo
-
 bpm = 1
 
 s0.initScreen()
-s1.initScreen()
-s2.initVideo("https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExb2lnaHl1c2hmNTg2aGt0YjI2ZGpqZmVuOGdrNXF0M21meWdmOWtuZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/7QCNgngVZFLrclmqMb/giphy.mp4")
-s3.initVideo("https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExc3FjazF5c3czeDRuc3E1MzdyczEyMmo5YW82eGJ5ajdsYmFrb20yOSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/myS6lWkJ1XFOZXDC3l/giphy.mp4")
 
 const f = () => a.fft[0]
 const arr4 = [1, 2, 3, 4]
@@ -17,23 +13,27 @@ const arm = arr4.map(e => e * Math.random(Math.PI))
 
 src(s0)
 	.scale(arm.ease('sin'))
-	.diff(src(s0)
+	.diff(src(o0)
 		.scale(0.9))
 	.out(o0)
 render(o0)
 
-src(s1)
-	.scale(arm.ease('sin'))
-	.diff(src(s1)
-		.scale(0.9))
+src(s0)
+	.pixelate(50, 50)
 	.out(o1)
-render()
+render(o1)
 
-
-src(s2)
-	.repeat(arm, arr3)
-	.diff(src(s2)
+src(o0)
+	.scale(arm.ease('sin'))
+	.diff(src(o0)
 		.scale(0.9))
+	.pixelate(50, 50)
 	.out(o2)
-render(o0)
+render(o2)
 
+src(o0)
+	.blend(o1, f * 50)
+	.blend(o2, f / 100)
+	.out(o3)
+
+render(o0)
